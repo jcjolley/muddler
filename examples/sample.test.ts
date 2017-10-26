c8ea8cc0e0612685152c52c46af6e0809df26a0c
Joshua Jolley <jolleyboy@gmail.com>
2017-10-25 22:34:11 -0600

    Updated sample test to be slightly more explicit

--- examples/sample.test.ts
+++ examples/sample.test.ts
@@ -1,8 +1,11 @@
-require("chai").should()
+import chai = require("chai")
+const should = chai.should();
+
 const fs = require('fs');
 declare var muddled: Function;
 
 const window = {};
+                                                                          
 const code = fs.readFileSync('examples/sample.temp.js', 'utf8').toString()
 eval(code);
 
@@ -23,7 +26,8 @@ describe('Sample', () => {
     describe('log', () => {
         it('should add an item to the log', () => {
             const res = muddled()
-            log_box.length.should.equal(1)
+            log_box.length.should.be.above(0)
+            log_box[0].should.equal('testing')
         })
     })
 })
