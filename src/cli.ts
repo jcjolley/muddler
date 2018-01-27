72a79abff7bf9e69d833f7daf3e1a5dce65fa442
Mister 4 Eyes <mister4eyez@gmail.com>
2018-01-26 19:30:10 -0500

    Cleaned up cli.ts and utils.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,21 @@
 import program = require('commander');
-import { muddleStr } from './utils'
+import { muddleStr } from './utils';
 import path = require('path');
 import fs = require('fs');
-const pjson = require('../package.json')
+const pjson = require('../package.json');
+
+export interface MuddleArgs {
+	outFile?:string,
+	watch?:boolean,
+	watchDir?:string,
+	outDir?:string,
+	config?:string,
+	verbose?:boolean,
+	quiet?:boolean,
+	testDir?:string,
+	reporter?:string,
+	skipTests?:boolean
+}
 
 export function parseArgs() {
     program.version(pjson.version)
@@ -20,18 +33,39 @@ export function parseArgs() {
 
         .parse(process.argv);
 
-    if (program.verbose && program.quiet) {
-        console.log("Be quiet, AND be verbose, eh?  You're " + muddleStr('drunk') + ".  Go home.")
+	//We need to wrap the arguments into something that can be read via type.
+	let args:MuddleArgs = {
+		outFile:program.outFile,
+		watch:program.watch,
+		watchDir:program.watchDir,
+		outDir:program.outDir,
+		verbose:program.verbose,
+		quiet:program.quiet,
+		skipTests:program.skipTests,
+		testDir:program.testDir,
+		reporter:program.reporter
+	};
+	
+    if (args.verbose && args.quiet) {
+        console.log("Be quiet, AND be verbose, eh?  You're " + muddleStr('drunk') + ".  Go home.");
         process.exit(1);
     }
 
     const configFilename = program.config ? path.normalize(program.config) : path.join(process.cwd(), 'muddle.json')
 
-    if (fs.existsSync(configFilename)) {
-        const config = JSON.parse(fs.readFileSync(configFilename, 'utf8'));
+	/*
+	 * Why access the file first then catch the an exception if it dosen't exist?
+	 * Well checking for existance before opening a file is an antipattern.
+	 * Code takes time to execute so the time between fs.existsSync and require(configFilename) it could be deleted by another process.
+	 * Thus lo and behold, you get an exception.
+	 * So instead of relying on the fs.existsSync, if it exists, you get the data without having to do another call and worse case scenerio you have to catch an exception which isn't computationally expensive on it's own.
+	 */
+	try {
+		const config = require(configFilename);
         const settings = { ...config, ...program };
         return settings;
-    }
-
-    return program;
+	}
+	catch(err) {
+		return program;
+	}
 }
