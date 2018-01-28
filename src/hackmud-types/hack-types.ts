972d9ce5cab6630959ce7da077c5ea04dea2f792
Mister 4 Eyes <mister4eyez@gmail.com>
2018-01-28 12:31:18 -0500

    Added hackmud typings.

--- src/hackmud-types/hack-types.ts
+++ src/hackmud-types/hack-types.ts
@@ -1,16 +1,108 @@
-//TODO: Find out how to make good types for these.
-export const $s:any = {};
-export const $fs:any = {};
-export const $ms:any = {};
-export const $ls:any = {};
-export const $ns:any = {};
-export const $4s:any = {};
-export const $3s:any = {};
-export const $2s:any = {};
-export const $1s:any = {};
+export interface ScriptReturn {
+	ok:boolean,
+	msg:string
+}
 
 //Out of Context error.
 const OocError = Error('You don\'t execute this code here silly. :)');
+
+class accts{
+	balance():string{throw OocError;}
+	transactions(args:{count:'all'|number, start?:number, to?:string, from?:string, script?:string}):string{throw OocError;}
+	xfer_gc_to(args:{to:string, ammount:number|string, memo?:string}):string{throw OocError;}
+}
+
+let scriptorTarget = {};
+
+function generateSecondHandler(args?:string[]){
+	if(args) {
+		return {
+			get(target:any, propKey:string) {
+				if(args.indexOf(propKey) === -1) {
+					throw Error("Not a function.");
+				}
+				else {
+					return (args?:object)=>{};
+				}
+			}
+		}
+	}
+	else {
+		return {
+			get(target:any, propKey:string){
+				return (args?:object)=>{};
+			}		
+		}
+	}
+}
+
+let scriptorHandler = {
+	get(target:any, propKey:string) {
+		switch(propKey){
+			case 'accts':
+			return new Proxy(target, generateSecondHandler(['balance', 'transactions', 'xfer_gc_to', 'xfer_gc_to_caller']));
+			case 'autos':
+			return new Proxy(target, generateSecondHandler(['balance']));
+			case 'chats':
+			return new Proxy(target, generateSecondHandler(['channels', 'create', 'join', 'leave', 'send', 'tell', 'users']));
+			case 'corps':
+			return new Proxy(target, generateSecondHandler(['create', 'hire','manage','offers','quit','top']));
+			case 'escrow':
+			return new Proxy(target, generateSecondHandler(['charge', 'confirm', 'stats']));
+			case 'kernel':
+			return new Proxy(target, generateSecondHandler(['hardline']));
+			case 'market':
+			return new Proxy(target, generateSecondHandler(['browse', 'buy', 'sell', 'stats']));
+			case 'scripts':
+			return new Proxy(target, generateSecondHandler(['ensure_highsec', 'ensure_lowsec', 'ensure_midsec', 'ensure_nullsec', 'fullsec', 'get_access_level', 'get_level', 'highsec', 'midsec', 'nullsec', 'sys', 'trust', 'user']));
+			case 'sys':
+			return new Proxy(target, generateSecondHandler(['access_log', 'breach', 'cull', 'init', 'loc', 'manage', 'specs', 'status', 'upgrade_log', 'upgrades', 'xfer_upgrade_to']));
+			case 'users':
+			return new Proxy(target, generateSecondHandler(['active', 'last_action', 'top']));
+			default:
+			return new Proxy(target, generateSecondHandler());
+		}
+	}
+}
+
+const Scriptor = new Proxy(scriptorTarget, scriptorHandler);
+
+/**
+ * NULLSEC
+ */
+export const $s  = Scriptor;
+/**
+ * FULLSEC
+ */
+export const $fs = Scriptor;
+/**
+ * HIGHSEC
+ */
+export const $ms = Scriptor;
+/**
+ * LOWSEC
+ */
+export const $ls = Scriptor;
+/**
+ * NULLSEC
+ */
+export const $ns = Scriptor;
+/**
+ * FULLSEC
+ */
+export const $4s = Scriptor;
+/**
+ * HIGHSEC
+ */
+export const $3s = Scriptor;
+/**
+ * LOWSEC
+ */
+export const $2s = Scriptor;
+/**
+ * NULLSEC
+ */
+export const $1s = Scriptor;
 
 /**
  * The final output of $db.f
@@ -34,14 +126,13 @@ interface updateOper {
 
 export class $db {
 	/**
-	 * Creates inserts a new document or documents.
+	 * Inserts a new document or documents.
 	 * @param obj An object or array of objects to pass.
 	 */
 	static i(obj:object|object[]):void{}
 
 	/**
 	 * Removes any documents matching the query
-	 * @param obj
 	 */
 	static r(query:object):void{}
 
@@ -52,7 +143,6 @@ export class $db {
 
 	/**
 	 * Updates any pre-existing documents matching the query.
-	 * @param query 
 	 */
-	static u(query, update:{upOp:updateOper}):void{}
+	static u(query:object, update:{upOp:updateOper}):void{}
 }
