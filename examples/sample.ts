513b25b2a1db01b7134c17d07d7daa073afb1c3e
Mister 4 Eyes <mister4eyez@gmail.com>
2018-01-28 05:45:08 -0500

    Updated sample to a format that can be changed but is TSC complient.

--- examples/sample.ts
+++ examples/sample.ts
@@ -1,6 +1,8 @@
-function (context, args) {
-    const l = #fs.scripts.lib();
-    const r = #db.i({"SID":"haxor"});
-    l.log('testing')
-    return l.get_log()
+import { $fs, $db } from '../src/hackmud-types/hack-types';
+
+function main(context, args) {
+    const l = $fs.scripts.lib();
+    const r = $db.i({"SID":"haxor"});
+    l.log('testing');
+    return l.get_log();
 }
