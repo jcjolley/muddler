5fecab41cbc1a576b3416096a9a34bb3e2d692a0
Mister 4 Eyes <mister4eyez@gmail.com>
2018-02-03 16:19:28 -0500

    Updated typescript library.

--- src/hackmud-types/hack-types.ts
+++ src/hackmud-types/hack-types.ts
@@ -1,18 +1,21 @@
 import { isObject, isNumber, isArray, isFunction } from "util";
 
+export interface ScriptContext {
+	caller:string;
+	this_script:string;
+	calling_script:string;
+	is_scriptor:boolean;
+	cols?:number;
+}
+
+
 export interface ScriptReturn {
-	ok:boolean,
-	msg:string
+	ok:boolean;
+	msg?:string;
 }
 
 //Out of Context error.
 const OocError = Error('You don\'t execute this code here silly. :)');
-
-class accts{
-	balance():string{throw OocError;}
-	transactions(args:{count:'all'|number, start?:number, to?:string, from?:string, script?:string}):string{throw OocError;}
-	xfer_gc_to(args:{to:string, ammount:number|string, memo?:string}):string{throw OocError;}
-}
 
 let scriptorTarget = {
 };
@@ -40,36 +43,18 @@ function generateSecondHandler(args?:string[]){
 }
 
 let scriptorHandler = {
-	get(target:any, propKey:string) {
-		switch(propKey){
-			case 'accts':
-			return new Proxy(target, generateSecondHandler(['balance', 'transactions', 'xfer_gc_to', 'xfer_gc_to_caller']));
-			case 'autos':
-			return new Proxy(target, generateSecondHandler(['balance']));
-			case 'chats':
-			return new Proxy(target, generateSecondHandler(['channels', 'create', 'join', 'leave', 'send', 'tell', 'users']));
-			case 'corps':
-			return new Proxy(target, generateSecondHandler(['create', 'hire','manage','offers','quit','top']));
-			case 'escrow':
-			return new Proxy(target, generateSecondHandler(['charge', 'confirm', 'stats']));
-			case 'kernel':
-			return new Proxy(target, generateSecondHandler(['hardline']));
-			case 'market':
-			return new Proxy(target, generateSecondHandler(['browse', 'buy', 'sell', 'stats']));
-			case 'scripts':
-			return new Proxy(target, generateSecondHandler(['ensure_highsec', 'ensure_lowsec', 'ensure_midsec', 'ensure_nullsec', 'fullsec', 'get_access_level', 'get_level', 'highsec', 'midsec', 'nullsec', 'sys', 'trust', 'user']));
-			case 'sys':
-			return new Proxy(target, generateSecondHandler(['access_log', 'breach', 'cull', 'init', 'loc', 'manage', 'specs', 'status', 'upgrade_log', 'upgrades', 'xfer_upgrade_to']));
-			case 'users':
-			return new Proxy(target, generateSecondHandler(['active', 'last_action', 'top']));
-			default:
-			return new Proxy(target, generateSecondHandler());
-		}
+	get(target:object, propKey:string) {
+		return new Proxy(target, generateSecondHandler());
 	}
 }
 
 
-const Scriptor = new Proxy(scriptorTarget, scriptorHandler);
+const Scrpr = new Proxy(scriptorTarget, scriptorHandler);
+
+export interface Scriptor {
+	call(obj?:object):string;
+}
+
 
 function shuffle(array: any[]): any[] {
 	var currentIndex: number = array.length, temporaryValue, randomIndex;
@@ -89,302 +74,227 @@ function shuffle(array: any[]): any[] {
 
 	return array;
 }
+
 let log:string[] = [];
-const FSScriptor = {
+
+interface lockSimInterface {
+	locks?:string|string[]
+}
+/**
+ * Decreped NULLSEC
+ * @deprecated
+ */
+export const $s  = Scrpr;
+
+interface MarketBrowseArgs {
+	i?:string|string[]
+	tier?:1|2|3|4,
+	listed_before?:number,
+	listed_after?:number,
+	rarity?:0|1|2|3|4|5,
+	cost?:number|string,
+	name?:string,
+	type?:'lock'|'script_space'|'script',
+	class?:'architect'|'executive'|'infiltrator'|'scavenger',
+
+	//TODO: get all chars objects mapped
+	chars?:object
+}
+/**
+ * Typed FULLSEC
+ */
+
+interface ScriptLib {
+	corruption_chars:string;
+	colors:string;
+	corruptions: number[];
+	security_level_names: string[];
+	math: {};
+	json: {};
+}
+export const $fs = {
+	/**
+	 * This is a collection of usefull scripts to use in yours!
+	 */
 	scripts: {
-		/**
-		 * Helper method to save chars.
-		 */
-		ok:()=>{return {ok:true}},
+		lib():ScriptLib{throw OocError}
+	},
 
+	dtr: {
 		/**
-		 * Helper method to save chars.
+		 * A simulation of teir 1 locks.
 		 */
-		not_impl:()=>{return {ok: false, msg:'not implemented'}},
+		t1_lock_sim:(args?:lockSimInterface)=>{}
+	},
 
+	accts: {
 		/**
-		 * Pushes a string representation of a value onto an array of log messages.
+		 * Transfers GC from user to caller.
+		 * @param args.ammount The ammount being transfered
+		 * @param args.memo The memo to be sent. Between 1-50 chars. Optional.
 		 */
-		log:(msg:string):void=>{log.push(msg)},
+		xfer_gc_to_caller(args:{ammount:string|number, memo?:string}){
+			throw OocError;
+		}
+	},
 
+	chats: {
 		/**
-		 * Used to print messages saved using the `log` function.
+		 * Creates a channel
+		 * @param args.name The name of the channel
 		 */
-		get_log:():string[]=>{return log},
-
-		/**
-		 * Returns a random integer between min and max.
-		 */
-		rand_int:(min:number, max:number):number=>{return Math.round((Math.random()*(max-min))+min)},
-
-		/**
-		 * Tests whether id1 and id2 values are equal.
-		 */
-		are_ids_eq:(id1:string, id2:string):boolean=>{return Math.random()>0.5}, //We use Math.random()>0.5 just in case any optimizers find this.
+		create(args:{name:string}){throw OocError;},
 
 		/**
-		 * Returns true if it's an object
+		 * Sends a message to a channel.
+		 * @param args.channel
+		 * @param args.msg
 		 */
-		is_obj:(any:any)=>{return isObject(any)},
+		send(args:{channel:string, msg:string}){throw OocError;},
 
 		/**
-		 * Returns true if it's a number.
+		 * Sends a message to somebody.
+		 * @param args.to The reciever
+		 * @param args.msg
 		 */
-		is_num:(any:any)=>{return isNumber(any)},
+		tell(args:{to:string, msg:string}){throw OocError;}
+	},
 
+	corps: {
 		/**
-		 * Returns true if it's an integer.
+		 * Creates a new organization
+		 * @param args.name The name of the organization
 		 */
-		is_int:(any:any)=>{return isNumber(any)&&Math.floor(any)===any},
+		create(args:{name:string}){throw OocError;},
+		//TODO: Make types for the rest of the coorp commands
+		hire(args:object){throw OocError;},
+		manage(args:object){throw OocError;},
+		offers(args:object){throw OocError;},
+		quit(args:object){throw OocError;},
+		top(args:object){throw OocError;}
+	},
 
+	kernel: {
 		/**
-		 * Returns true if it's negative.
+		 * Activates hardline mode.
+		 * @param args.dc Disconnect from the hardline. Optional
 		 */
-		is_neg:(any:any)=>{return isNumber(any)&&any<0},
+		hardline(args?:{dc:boolean}){throw OocError;}
+	},
 
+	market: {
 		/**
-		 * Returns true if it's an array.
+		 * Browses the market
 		 */
-		is_arr:(any:any)=>{return isArray(any)},
-
-		/**
-		 * Returns true if it's a function.
-		 */
-		is_func:(any:any)=>{return isFunction(any)},
-
-		/**
-		 * Returns true if it's defined.
-		 */
-		is_def:(any:any):boolean=>{return Math.random()>0.5},
-
-		/**
-		 * Returns true if it's a valid user/script.
-		 */
-		is_valid_name:(any:any)=>{return Math.random()>0.5},
-
-		/**
-		 * Returns string representation of object.
-		 */
-		dump:(obj:object)=>{return JSON.stringify(obj)},
-
-		/**
-		 * Returns a clone of the object.
-		 */
-		clone:(obj:object)=>{return Object.assign({}, obj)},
-
-		/**
-		 * Merges the contents of object 2 into object 1.
-		 */
-		merge:(ob1:object, ob2:object):void=>{},
-
-		/**
-		 * Returns an array containing the values of the object's properties.
-		 */
-		get_values:(obj:object):any[]=>{return []},
-
-		/**
-		 * Returns a nuber calculated based on the stirng argument.
-		 */
-		hash_code:(str:string):number=>{return Number.NaN},
-
-		/**
-		 * Converts raw number to a GC string.
-		 */
-		to_gc_str:(gc:number):string=>{return ''},
-
-		/**
-		 * Converts GC string to a raw number.
-		 */
-		to_gc_num:(gc:string):number=>{return Number.NaN},
-
-		/**
-		 * Converts a Date object to a string usable by the game.
-		 */
-		to_game_timestr:(date:Date):string=>{return date.toString()},
-
-		/**
-		 * Truncates the given string to the given length if it's longer than len.
-		 */
-		cap_str_len:(str:string, len:number):string=>{return str.substr(0, len)},
-
-		/**
-		 * Returns a selection of elements matching the fn predicate.
-		 */
-		each:<T>(arr:T[], fn:(key:number, T:any)=>boolean)=>{arr.forEach((value, index)=>fn(index, value))},
-
-		/**
-		 * Returns a number of items from array that matches the fn predicate.
-		 */
-		select:<T>(arr:T[], fn:(key:number, val:T)=>boolean):T[]=>{return arr.filter((value, index)=>fn(index, value))},
-
-		/**
-		 * Gets the number of elements that matches the fn predicate in 
-		 */
-		count:<T>(arr:T[], fn:(key:number, val:T)=>boolean):number=>{return arr.filter((value, index)=>fn(index, value)).length},
-
-		/**
-		 * Returns the first element that matches the predicate.
-		 * The array can either be of lenght 1 or 0.
-		 */
-		select_one:<T>(arr:T[], fn:(key:number, val:T)=>boolean):T[]=>{
-			let res = arr.find((value, index)=>fn(index, value));
-
-			//This is some weird syntax but it's whats required.
-			if(res) {
-				return [res];
-			}
-			else {
-				return [];
-			}
-		},
-
-		/**
-		 * Maps the array to a new structure defined by fn.
-		 */
-		map:<T, J>(arr:T[], fn:(key:number, val:T)=>J)=>{arr.map((value, index)=>fn(index, value))},
-		shuffle:<T>(arr:T[])=>shuffle(arr),
-
-		/**
-		 * Some sort of sorting function.
-		 * one > two => 1
-		 * one < two =>-1
-		 * one===two => 0
-		 */
-		sort_asc:(one:number, two:number): number=>{
-			if(one > two) {
-				return 1;
-			}
-			else if(one < two) {
-				return -1;
-			}
-			else {
-				return 0;
-			}
-		},
-
-		/**
-		 * Some sort of sorting function.
-		 * one < two => 1
-		 * one > two =>-1
-		 * one===two => 0
-		 */
-		sort_desc:(one:number, two:number): number=>{
-			if(one < two) {
-				return 1;
-			}
-			else if(one > two) {
-				return -1;
-			}
-			else {
-				return 0;
-			}
-		},
-		
-		/**
-		 * ???
-		 */
-		num_sort_asc:(...anything:any[]):any=>{return {}},
-
-		/**
-		 * ???
-		 */
-		num_sort_desc:(...anything:any[]):any=>{return {}},
-
-		/**
-		 * Gest the current date and adds add_ms to it.
-		 */
-		add_time:(date:Date, add_ms:number):Date=>{return new Date(date.getMilliseconds()+add_ms);},
-		
-		security_level_names:['NULLSEC', 'LOWSEC', 'MIDSEC', 'HIGHSEC', 'FULLSEC'],
-
-		/**
-		 * Gets the string of the security name from the level.
-		 */
-		get_security_level_name:(level:number)=>['NULLSEC', 'LOWSEC', 'MIDSEC', 'HIGHSEC', 'FULLSEC'][level],
-
-		/**
-		 * Creates a new random string.
-		 * The string consists of lowercaes alphanumeric characers.
-		 */
-		create_rand_string:(len:string)=>{return 'TODO: make a random string generator.'},
-
-		/**
-		 * Gets the user from a script name.
-		 */
-		get_user_from_script:(scriptName:any):string=>{return 'TODO: find out where these things.'},
-
-		u_sort_num_arr_desc:{} as any,
-
-		/**
-		 * Returns true if the script can continue after ms miliseconds.
-		 */
-		can_continue_execution:(ms:number):boolean=>{return true},
-
-		can_continue_execution_error:{} as any,
-
-		date:new Date(Date.now()),
-
-		/**
-		 * Gets the current date.
-		 */
-		get_date:()=>new Date(Date.now()),
-
-		/**
-		 * Gets the current time form the date.
-		 * Similar to Date.getTime()
-		 */
-		get_date_utcsecs:()=>new Date(Date.now()).getTime()
+		browse(args:MarketBrowseArgs){throw OocError;}
 	}
 };
 
-const FullsecScriptor = new Proxy(FSScriptor, scriptorTarget);
+interface TransactionArgs {
+	count?:'all'|number,
+	to?:string,
+	from?:string,
+	script?:string
+}
+/**
+ * Typed HIGHSEC
+ */
+export const $hs = {
+	accts: {
+		/**
+		 * Returns the balance of the user.
+		 * @returns A GC string representing how much GC the user has.
+		 */
+		balance():string{throw OocError;},
+
+		/**
+		 * Shows transactions that has taken place
+		 */
+		transactions(args:TransactionArgs):string{throw OocError;}
+	}
+};
+
+ /**
+  * Typed MIDSEC
+  */
+export const $ms = {
+	accts: {
+		/**
+		 * Transfer Gc to user
+		 * 
+		 * @param args.to User
+		 * @param args.ammout A GC string or a number in raw GC
+		 * @param args.memo The memo you want to give to the person. Between 1-50 chars. Optional.
+		 */
+		xfer_gc_to(args:{to:string, ammount:number|string, memo?:string}):string{throw OocError;}
+	},
+
+	chats: {
+		channels(args:object){throw OocError;},
+		join(args:object){throw OocError;},
+		leave(args:object){throw OocError;},
+		users(args:object){throw OocError;}
+	},
+
+	market: {
+		buy(args:object){throw OocError;},
+		stats(args:object){throw OocError;}
+	}
+};
 
 /**
- * NULLSEC
+ * Typed LOWSEC
  */
-export const $s  = Scriptor;
+export const $ls = {
+	market: {
+		sell(args:object){throw OocError;}
+	}
+};
+
 /**
- * FULLSEC
+ * Typed NULLSEC
  */
-export const $fs = FullsecScriptor;
+export const $ns = {
+	trust:{
+		/**
+		 * Possibly an easter egg.
+		 */
+		me:()=>{return "Thank You."}
+	}
+};
+
 /**
- * HIGHSEC
+ * Untyped FULLSEC
  */
-export const $ms = Scriptor;
+export const $4s = Scrpr;
+
 /**
- * LOWSEC
+ * Untyped HIGHSEC
  */
-export const $ls = Scriptor;
+export const $3s = Scrpr;
+
 /**
- * NULLSEC
+ * Untyped LOWSEC
  */
-export const $ns = Scriptor;
+export const $2s = Scrpr;
+
 /**
- * FULLSEC
+ * Untyped NULLSEC
  */
-export const $4s = Scriptor;
-/**
- * HIGHSEC
- */
-export const $3s = Scriptor;
-/**
- * LOWSEC
- */
-export const $2s = Scriptor;
-/**
- * NULLSEC
- */
-export const $1s = Scriptor;
+export const $1s = Scrpr;
 
 /**
  * The final output of $db.f
- * 
- * first() gets the first matching object from the database
- * 
- * array() gets all matching objects from the database
  */
 interface $findChain{
+	/**
+	 * Get the first maching object from the database.
+	 */
 	first():object;
+	
+	/**
+	 * Get all mathinc objects from the database.
+	 */
 	array():object[];	
 }
 
@@ -417,4 +327,49 @@ export class $db {
 	 * Updates any pre-existing documents matching the query.
 	 */
 	static u(query:object, update:{upOp:updateOper}):void{}
+}
+
+/**
+ * Specifies a time before the scripts start.
+ * Find out how much time you have left by doing `Date.now()-_START`
+ */
+export const _START = Date.now();
+/**
+ * The number of miliseconds a program has to run.
+ * Allways 5000 except for trust scripts.
+ * Since you aren't trust, you are stuck with 5000.
+ */
+export const _TIMEOUT = 5000;
+
+/**
+ * Hackmud's global variable.
+ * Starts out empty but you can add to it if you like.
+ */
+export var $G = {};
+
+export const JSON = {
+	stringify(object:object, error?:any, replacer?:any, space?:string):string {
+		return '';
+	},
+
+	parse(string:string, error?:any, reviver?:any):object {
+		return {};
+	}
+}
+
+let executed = false;
+
+/**
+ * The Function Multi Call Lock (FMCL) prevents mutliple calls from one function.
+ * If it's executed once, it returns falsy, else it returns truthy.
+ */
+export var $FMCL = {
+	get() {
+		let temp = executed;
+		if(!executed){
+			executed = true;
+		}
+
+		return temp;
+	}
 }
